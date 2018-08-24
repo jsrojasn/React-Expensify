f6c9eea1ca3d4e1135fdb033c29b3d9b93e40ef3
Juan Rojas <jsrojasn@unal.edu.co>
2018-08-24 18:26:17 -0500

    Help page deleted

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -15,7 +15,7 @@ const AppRouter= () => (
                 <Route path="/" component={ExpenseDashBoardPage} exact={true} />
                 <Route path="/create" component={AddExpensePage} />
                 <Route path="/edit/:id" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
+                
                 <Route component={NotFoundPage} />
             </Switch>
         </div>
