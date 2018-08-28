4d16e62c63920ccef442001f5e8f3f7082f5b0d2
Juan Rojas <jsrojasn@unal.edu.co>
2018-08-27 20:21:58 -0500

    store user data in user area

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -9,10 +9,11 @@ export const addExpense = (expense) => ({
 })
 
 export const startAddExpense = (expenseData={})=>{
-    return (dispatch)=>{
+    return (dispatch, getState)=>{
+        const uid = getState().auth.uid
         const { description = "", note = "", amount = 0, createdAt = 0} = expenseData;
         const expense = {description,note, amount, createdAt}
-        dataBase.ref("expenses").push(expense).then((ref)=>{
+        dataBase.ref(`users/${uid}/expenses`).push(expense).then((ref)=>{
             dispatch(addExpense({
                 id: ref.key,
                 ...expense
@@ -25,9 +26,10 @@ export const removeExpense = (expense) => ({
     id: expense.id
 })
 export const startRemoveExpense = (expenseData)=>{
-    return (dispatch)=>{
+    return (dispatch, getState)=>{
+        const uid = getState().auth.uid
         const id=expenseData.id
-        return dataBase.ref(`expenses/${id}`).remove().then((ref)=>{
+        return dataBase.ref(`users/${uid}/expenses/${id}`).remove().then((ref)=>{
             dispatch(removeExpense({
                 id
             }))
@@ -40,8 +42,9 @@ export const editExpense = (id, updates) => ({
     updates
 })
 export const startEditExpense = (id, updates)=>{
-    return (dispatch)=>{
-        dataBase.ref(`expenses/${id}`).update(updates).then(()=>{
+    return (dispatch,getState)=>{
+        const uid = getState().auth.uid
+        dataBase.ref(`users/${uid}/expenses/${id}`).update(updates).then(()=>{
             dispatch(editExpense(id, updates))
         })
     }
@@ -55,8 +58,9 @@ export const setExpenses =(expenses)=>({
 
 
 export const startSetExpenses = ()=>{
-    return (dispatch)=>{
-        return dataBase.ref("expenses")
+    return (dispatch, getState)=>{
+        const uid=getState().auth.uid
+        return dataBase.ref(`users/${uid}/expenses`)
         .once("value")
         .then((snapshot)=>{
             const expenses = []
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,13 +7,14 @@ import EditExpensePage from "../components/EditExpensePage"
 import LoginPage from "../components/LoginPage";
 import NotFoundPage from "../components/NotFoundPage"
 import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute"
 export const history = createHistory()
 
 const AppRouter= () => (
     <Router history={history}>
         <div>
         <Switch>                
-                <Route path="/" component={LoginPage} exact={true} />
+                <PublicRoute path="/" component={LoginPage} exact={true} />
                 <PrivateRoute path="/dashboard" component={ExpenseDashBoardPage}/>
                 <PrivateRoute path="/create" component={AddExpensePage} />
                 <PrivateRoute path="/edit/:id" component={EditExpensePage} />                
